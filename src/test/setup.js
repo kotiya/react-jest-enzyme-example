fe75039344542753dbdcf6f45dde76a67474ec63
Weiran Li <weiran@second.dev>
2023-09-12 20:55:25 -0700

    Second[Agent] enzyme to react testing library

--- src/test/setup.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file Tests setup.
- */
-
-import '@babel/polyfill';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
