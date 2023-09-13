ab1809c4281038cce2a1c849ce440d9d1defa389
Weiran Li <weiran@second.dev>
2023-09-12 21:22:57 -0700

    Second[Agent] enzyme to react testing library

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -1,19 +1,12 @@
-/**
- * @file Test suite for the Calculator component.
- */
-
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { mount, shallow } from 'enzyme';
+import { render, fireEvent } from '@testing-library/react';
 
 import Calculator, { getSum } from '../components/calculator';
 
 describe('Calculator component', () => {
   it('should render snapshot', () => {
-    const component = renderer.create(<Calculator />);
-
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    const { container } = render(<Calculator />);
+    expect(container).toMatchSnapshot();
   });
 
   it('should return the correct sum', () => {
@@ -22,25 +15,23 @@ describe('Calculator component', () => {
   });
 
   it('should render required form elements', () => {
-    const calculator = shallow(<Calculator />);
-
-    const form = calculator.find('form');
-    expect(form.length).toBe(1);
-    expect(form.find('input').length).toBe(2);
-    expect(form.find('button').length).toBe(1);
-    expect(form.find('p.result').length).toBe(1);
+    const { container } = render(<Calculator />);
+    const form = container.querySelector('form');
+    expect(form).toBeInTheDocument();
+    expect(form.querySelectorAll('input').length).toBe(2);
+    expect(form.querySelectorAll('button').length).toBe(1);
+    expect(form.querySelectorAll('p.result').length).toBe(1);
   });
 
   it('should display the result on add', () => {
-    const calculator = mount(<Calculator />);
+    const { container } = render(<Calculator />);
+    const form = container.querySelector('form');
 
-    const form = calculator.find('form');
+    form.querySelector('input').value = 3;
+    form.querySelectorAll('input')[1].value = 5;
+    fireEvent.click(form.querySelector('button'));
 
-    form.childAt(0).instance().value = 3;
-    form.childAt(1).instance().value = 5;
-    form.find('button').simulate('click');
-
-    const result = calculator.find('.result');
-    expect(result.text()).toEqual('8');
+    const result = container.querySelector('.result');
+    expect(result.textContent).toEqual('8');
   });
 });
